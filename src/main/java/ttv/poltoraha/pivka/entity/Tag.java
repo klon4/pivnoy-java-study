61b0ed96f6491c9b8d3fc4886009bedc3e5bdca3
klon4 <klonu4@gmail.com>
2025-08-19 14:24:11 +0300

    Наверное лучше сделать сязь много ко многим но тогда нужно трограть цитаты, поэтому пока такая странная связь

--- src/main/java/ttv/poltoraha/pivka/entity/Tag.java
+++ src/main/java/ttv/poltoraha/pivka/entity/Tag.java
@@ -1,5 +1,21 @@
 package ttv.poltoraha.pivka.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity(name = "tag")
+@Data
 public class Tag {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @OneToMany
+    private List<Quote> quotes = new ArrayList<>();
 }
---
+++ src/main/java/ttv/poltoraha/pivka/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package ttv.poltoraha.pivka.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ttv.poltoraha.pivka.entity.Tag;
+
+@Repository
+public interface TagRepository extends CrudRepository <Tag, Integer> {
+}
